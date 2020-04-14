a214dd1766d035b4b4084c5264c4fd025b05ea3e
dylan@dylanhulbert.com <dylan@dylanhulbert.com>
2020-04-14 15:38:16 -0700

    added view queries

--- server.js
+++ server.js
@@ -13,4 +13,51 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  runApp();
 });
+
+function runApp() {
+  inquirer.prompt({
+    name: "selection",
+    type: "list",
+    choices: ["View Departments","View Roles", "View Employees", "Exit"]
+  }).then(answer => {
+        switch (answer.selection) {
+          case "View Departments":
+          viewDepartments();
+          break;
+          case "View Roles":
+          viewRoles();
+          break;
+          case "View Employees":
+          viewEmployees();
+          break;
+          case "Exit":
+          process.exit();
+        }
+      })
+}
+
+function viewDepartments() {
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runApp();
+  })
+}
+
+function viewRoles() {
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runApp();
+  })
+}
+
+function viewEmployees() {
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runApp();
+  })
+}
